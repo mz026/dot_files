35e607529874228d4ccfc256962d06bc6d426bf1
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2014-07-10 18:33:43 +0800

    change zsh.sh repo to https

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -2,7 +2,7 @@
 
 oh_my_zsh=$HOME/.oh-my-zsh
 zshrc=$HOME/.zshrc
-git clone git@github.com:mz026/oh-my-zsh.git $oh_my_zsh && \
+git clone https://github.com/mz026/oh-my-zsh.git $oh_my_zsh && \
 cd $oh_my_zsh && git submodule init && git submodule update &&\
 echo 'source $HOME/.dot_files/zshrc' >> $zshrc
 
