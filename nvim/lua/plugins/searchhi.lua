fe5c6f03efe866c8682447275dda4a0eb7f97a6a
Yanghsing Lin <yanghsing.lin@gmail.com>
2024-02-15 22:31:24 +0800

    unmap searchhi gd

--- nvim/lua/plugins/searchhi.lua
+++ nvim/lua/plugins/searchhi.lua
@@ -5,20 +5,12 @@ return {
       nmap n <Plug>(searchhi-n)
       nmap N <Plug>(searchhi-N)
       nmap * <Plug>(searchhi-*)
-      nmap g* <Plug>(searchhi-g*)
       nmap # <Plug>(searchhi-#)
-      nmap g# <Plug>(searchhi-g#)
-      nmap gd <Plug>(searchhi-gd)
-      nmap gD <Plug>(searchhi-gD)
 
       vmap n <Plug>(searchhi-v-n)
       vmap N <Plug>(searchhi-v-N)
       vmap * <Plug>(searchhi-v-*)
-      vmap g* <Plug>(searchhi-v-g*)
       vmap # <Plug>(searchhi-v-#)
-      vmap g# <Plug>(searchhi-v-g#)
-      vmap gd <Plug>(searchhi-v-gd)
-      vmap gD <Plug>(searchhi-v-gD)
 
       " ,/ to close search hilight
       nmap <leader>/ <Plug>(searchhi-clear-all)
